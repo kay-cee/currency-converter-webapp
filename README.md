6a94a43e86ec3b50d970f119b13ff0c3c36ff9df
kay-cee <anikenechi.c@gmail.com>
2018-07-02 00:20:35 -0400

    first push

---
+++ README.md
@@ -0,0 +1,8 @@
+Currency-Converter web-App
+
+Using the currency API from https://free.currencyconverterapi.com
+
+7DaysOfCodeChallenge By  ALC 
+
+
+
---
+++ service-worker.js
@@ -0,0 +1,28 @@
+let cacheName = 'currency_converter_cache';
+
+let cacheFiles =[
+    './',
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './js/currency.js'
+];
+
+self.addEventListener('install', function(e){
+    console.log("{Service Worker}: Installed");
+    e.waitUntil (
+        caches.open(cacheName)
+        .then(function(cache){
+            console.log("{Service Worker}: Caching Files...");
+         return cache.addAll(cacheFiles);
+        })
+    )
+});
+
+self.addEventListener('activate', function(e){
+    console.log("{Service Worker}: Activated")
+});
+
+self.addEventListener('fetch', function(e){
+     console.log("{Service Worker}: Installed",e.request.url);
+});
